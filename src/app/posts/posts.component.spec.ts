25eb0c7c03ab6b36e1d417c6efae08c0f53927a0
Ivan Pachesnyi <32592566+pachesnyi@users.noreply.github.com>
2024-01-04 19:03:32 +0100

    Handle inner post state and unit tests (#4)

--- src/app/posts/posts.component.spec.ts
+++ src/app/posts/posts.component.spec.ts
@@ -1,17 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PostsComponent } from './posts.component';
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { PostService } from "./services/post.service";
+import { PostStore, SelectedPostStore } from "./stores/posts.store";
 
 describe('PostsComponent', () => {
   let component: PostsComponent;
   let fixture: ComponentFixture<PostsComponent>;
+  let postService: jasmine.SpyObj<PostService>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PostsComponent]
+      imports: [PostsComponent, HttpClientTestingModule],
+      providers: [
+        { provide: PostService, useValue: postService },
+        PostStore,
+        SelectedPostStore,
+      ],
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(PostsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +27,15 @@ describe('PostsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('postSelection should update store with emitted post', () => {
+    const post = {id: 1, body: "Lorem ipsum", userId: 2, title: "Lorem"};
+
+    spyOn(component.selectedPostStore, 'setSelected');
+
+    component.postSelection(post);
+
+    expect(component.selectedPostStore.setSelected).toHaveBeenCalledWith(post);
   });
 });
